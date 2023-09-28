9654d55b162921695c357e9025ada0b546f05abf
caspermike997 <caspermike97@gmail.com>
2023-09-28 19:04:16 +0100

    first commit

---
+++ src/db/db.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {AiF}
+
+const db = () => {
+
+
+    const data = [
+        {
+            img: ""
+        }
+    ]
+
+
+
+
+  return (
+    <div>db</div>
+  )
+}
+
+export default db
